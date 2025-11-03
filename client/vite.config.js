83d3c1e641d2be2d5e70e3bbd2775ce20ad382f6
Amrit Raj <150701418+amritraj006@users.noreply.github.com>
2025-11-03 10:26:52 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,15 +5,4 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      // 🛠️ When frontend calls `/api`, forward it to backend
-      '/api': {
-        target: 'https://real-time-vehicle-tracking.onrender.com', // your backend server
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ''), // remove `/api` prefix
-      },
-    },
-  },
 })
