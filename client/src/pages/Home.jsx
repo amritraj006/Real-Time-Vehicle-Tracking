cde9b3d6c83135e08d591bafd72508550c343228
Amrit Raj <150701418+amritraj006@users.noreply.github.com>
2025-12-16 10:33:03 +0530

    latest commit

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,10 +4,12 @@ import Features from '../components/home/Features'
 import Testimonial from '../components/home/Testimonial'
 import CallToAction from '../components/home/CallToAction'
 import Footer from '../components/home/Footer'
+import Banner from '../components/home/Banner'
 
 const Home = () => {
   return (
     <div>
+      <Banner />
       <Hero />
       <Features />
       <Testimonial />
