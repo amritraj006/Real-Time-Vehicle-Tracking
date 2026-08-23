7dbde004426aa38292aa57fdd1da332a965c891f
Amrit Raj <150701418+amritraj006@users.noreply.github.com>
2026-08-23 19:21:38 +0530

    refactor: remove UserDashboard page and corresponding route from App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,6 @@ import MapView from "./pages/MapView";
 import Lander from "./pages/Lander";
 import About from "./pages/About";
 import Community from "./pages/Community";
-import UserDashboard from "./pages/UserDashboard";
 import NotFound from "./components/common/NotFound";
 
 const App = () => {
@@ -18,7 +17,6 @@ const App = () => {
       <Route path="/track/:vehicleId" element={<Lander />} />
       <Route path="/about" element={<About />} />
       <Route path="/community" element={<Community />} />
-      <Route path="/user-dashboard" element={<UserDashboard />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
